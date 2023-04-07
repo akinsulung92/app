702caad171545f417184be9d55f60910b003e61f
manbungsu9 <130164951+manbungsu9@users.noreply.github.com>
2023-04-07 22:22:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask,request
 import os
 import subprocess
 
@@ -13,7 +13,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Welcome, this is a Flask app deployed on Zeabur"
- 
+
+@app.route('/execute', methods=['GET'])
+def execute_command():
+    command = request.args.get('command')
+    if not command:
+        return 'No command provided', 400
+
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+
+    def generate_output():
+        for line in process.stdout:
+            yield line
+
+    return Response(generate_output(), mimetype='text/plain')
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
     
